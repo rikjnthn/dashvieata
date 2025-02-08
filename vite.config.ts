2b4311eb71c782bd12e3746c4e2db68a4e14ef33
Erik <100293508+rikjnthn@users.noreply.github.com>
2025-02-08 20:26:56 +0700

    feat: add setup files

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: ["./setup.ts"],
+  },
 });
