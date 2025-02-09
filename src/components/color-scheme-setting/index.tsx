b1b02403002f2362fe8de66dae9142278457d3c8
Erik <100293508+rikjnthn@users.noreply.github.com>
2025-02-09 12:25:54 +0700

    refactor(color-scheme-setting, font-size-setting): refactor code

--- src/components/color-scheme-setting/index.tsx
+++ src/components/color-scheme-setting/index.tsx
@@ -1,12 +1,11 @@
+import { COLORSCHEME } from "../../constant/setting";
 import Dropdown from "../dropdown";
-
-const colorSchemes = ["Light", "Dark"];
 
 const ColorSchemeSetting = () => {
   return (
     <div className="max-w-100 px-4 py-2.5">
       <div className="mb-2.5 px-4 py-1 text-xl font-medium">Color scheme</div>
-      <Dropdown defaultValue="Light" options={colorSchemes} />
+      <Dropdown defaultValue="Light" options={COLORSCHEME} />
     </div>
   );
 };
--- src/components/font-size-setting/index.tsx
+++ src/components/font-size-setting/index.tsx
@@ -1,26 +1,11 @@
+import { FONTSIZES } from "../../constant/setting";
 import Dropdown from "../dropdown";
-
-const fontSizes = [
-  "12px",
-  "13px",
-  "14px",
-  "15px",
-  "16px",
-  "17px",
-  "18px",
-  "19px",
-  "20px",
-  "21px",
-  "22px",
-  "23px",
-  "24px",
-];
 
 const FontSizeSetting = () => {
   return (
     <div className="max-w-100 px-4 py-2.5">
       <div className="mb-2.5 px-4 py-1 text-xl font-medium">Font Size</div>
-      <Dropdown defaultValue="16px" options={fontSizes} />
+      <Dropdown defaultValue="16px" options={FONTSIZES} />
     </div>
   );
 };
