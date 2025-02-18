22bcac131a8b206a131899021cdcf63a89feccbf
Erik <100293508+rikjnthn@users.noreply.github.com>
2025-02-18 11:44:18 +0700

    1. feat(messages page): add responsiveness 2. feat: update style

--- src/components/header-nav/index.tsx
+++ src/components/header-nav/index.tsx
@@ -1,6 +1,6 @@
 const HeaderNav = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex h-17.5 w-full items-center justify-between px-2.5 max-xl:pl-16">
+    <div className="flex min-h-17.5 w-full items-center justify-between px-2.5 max-xl:pl-16">
       {children}
     </div>
   );
--- src/components/search-bar/index.tsx
+++ src/components/search-bar/index.tsx
@@ -10,11 +10,11 @@ const SearchBar = ({ setSearch }: SearchBarPropsType) => {
   return (
     <form
       onSubmit={(e) => e.preventDefault()}
-      className="inset-ring-grey-200/50 dark:inset-ring-grey-400/50 flex max-w-94.5 rounded-full py-1.25 pr-4 pl-6.5 inset-ring"
+      className="inset-ring-grey-200/50 dark:inset-ring-grey-400/50 flex w-full max-w-94.5 rounded-full py-1.25 pr-4 pl-6.5 inset-ring"
     >
       <input
         onInput={(e) => setSearch(e.currentTarget.value)}
-        className="input-dynamic-font-size placeholder:text-grey-400 text-black outline-0 dark:text-white"
+        className="input-dynamic-font-size placeholder:text-grey-400 w-full text-black outline-0 dark:text-white"
         placeholder="Search..."
         style={
           {
